8565a48396a5c14dd317a047b51fd1b42692b2af
Miguel <mcruzc1206@egresado.ipn.mx>
2026-04-29 01:19:04 +0000

    feat: se agreaga componente de inicio en seccion principal con simbolos matematicos

--- academicosweb/app/page.tsx
+++ academicosweb/app/page.tsx
@@ -1,10 +1,12 @@
 import Header from "@/components/Header";
+import Inicio from "@/app/components/Inicio";
 import Link from "next/link";
 
 export default function Home() {
   return (
     <main className="container">
       <Header />
+      <Inicio />
       <h1>Inicio</h1>
 
       <Link href="/perfil">
